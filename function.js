50e50d8086134929f9617aaa25630b7846b6af50
Orosz Zsombor Olivér <orosz.zsombor.oliver@burg.lo.hu>
2024-09-12 09:48:10 +0200

    Util function factorial

--- function.js
+++ function.js
@@ -1,10 +1,25 @@
 function getFirstElement (arr){
     return arr[0]
     }
-    
+export function factorial(n) {
+    let szam = n
+    for (let index = 0; index < n; index++) {
+
+        if (index!=0) {
+            szam = szam*index
+        } 
+
+        
+    }
+    return n
+}
+        
+        console.log(`Factorial of 5: ${factorial(5)}`);
+        
     const numbers = [1, 2, 3];
     const words = ["apple", "banana", "cherry"];
     
     console.log(`First number: ${getFirstElement(numbers)}`);
     console.log(`First word: ${getFirstElement(words)}`);
+    
     
